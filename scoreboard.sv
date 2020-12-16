e25e58f81421234d2b0ecf0c7c0073b10530d09e
Rishav Kumar <kumarrishav14@gmail.com>
2020-12-16 23:38:43 +0530

    Code refactored

--- scoreboard.sv
+++ scoreboard.sv
@@ -3,13 +3,18 @@ class scoreboard extends uvm_scoreboard;
     
     // Variable
     uvm_analysis_imp#(transaction, scoreboard) ap_export;
+    reference_model rm;
     transaction act_trans, exp_trans;
 
-    function void write(transaction act_trans);
-        this.act_trans = act_trans;
-        `uvm_info("scoreboard", $sformatf("Received Packet:: %s", act_trans.convert2string()), UVM_HIGH)
-        
+    function void check();
+        if (act_trans.compare(exp_trans)) begin
+            `uvm_info("SCB", $sformatf("%s\nStatus:: PASSED", act_trans.convert2string()), UVM_NONE)
+        end
+        else begin
+            `uvm_error("SCB", $sformatf("%s\nStatus:: FAILED", act_trans.convert2string()))
+        end
     endfunction
+    
     // Constructor:new
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -17,10 +22,23 @@ class scoreboard extends uvm_scoreboard;
 
     //  Function: build_phase
     extern function void build_phase(uvm_phase phase);
+
+    //  Function: write
+    extern function void write(transaction trans);
     
 endclass //scoreboard extends uvm_scoreboard
 
 function void scoreboard::build_phase(uvm_phase phase);
-
     ap_export = new("ap_export", this);
+    rm = reference_model::type_id::create("rm", this);
+    act_trans = new("act_trans");
 endfunction: build_phase
+
+function void scoreboard::write(transaction trans);
+    act_trans.copy(trans);
+    `uvm_info("scoreboard", $sformatf("Received Packet:: %s", act_trans.convert2string()), UVM_HIGH)
+    exp_trans = rm.get_ref_val(trans);
+    check();
+    // act_trans.print();
+    // exp_trans.print();
+endfunction: write
