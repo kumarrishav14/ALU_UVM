60c872bd580d4ad385abc9826af861f83bba9af7
Rishav Kumar <kumarrishav14@gmail.com>
2020-12-16 23:37:22 +0530

    Added file logging

--- environmnet.sv
+++ environmnet.sv
@@ -6,6 +6,7 @@ class environment extends uvm_env;
     random_seq rnd_seq;
     agent agnt;
     scoreboard scb;
+    int f_id;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -17,36 +18,49 @@ class environment extends uvm_env;
     //  Function: connect_phase
     extern function void connect_phase(uvm_phase phase);
 
-    function void end_of_elaboration_phase(uvm_phase phase);
-        super.end_of_elaboration_phase(phase);
-        uvm_top.print_topology();
-    endfunction: end_of_elaboration_phase
-    
+    //  Function: end_of_elaboration_phase
+    extern function void end_of_elaboration_phase(uvm_phase phase);  
 
     //  Function: run_phase
     extern task run_phase(uvm_phase phase);
-    
-    
+
+    //  Function: final_phase
+    extern function void final_phase(uvm_phase phase);
 endclass //environment extends uvm_environment
 
 function void environment::build_phase(uvm_phase phase);
     dir_seq = directed_seq::type_id::create("dir_seq");
-    //rnd_seq = random_seq::type_id::create("rnd_seq");
+    rnd_seq = random_seq::type_id::create("rnd_seq");
     agnt = agent::type_id::create("agnt", this);
     scb = scoreboard::type_id::create("scb", this);
 endfunction: build_phase
 
 function void environment::connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-
     agnt.mon.ap.connect(scb.ap_export);
 endfunction: connect_phase
+
+function void environment::end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
+    uvm_top.print_topology();
+    f_id = $fopen("log.txt", "w");
+    uvm_top.set_report_default_file_hier(f_id);
+    uvm_top.set_report_severity_action_hier(UVM_INFO, UVM_LOG);
+    uvm_top.set_report_severity_action_hier(UVM_ERROR, UVM_LOG);
+endfunction: end_of_elaboration_phase
+
 
 task environment::run_phase(uvm_phase phase);
     phase.raise_objection(this);
     dir_seq.start(agnt.seqr);
-    
+    // rnd_seq.start(agnt.seqr);
     phase.drop_objection(this);
 endtask: run_phase
+
+function void environment::final_phase(uvm_phase phase);
+    super.final_phase(phase);
+    $fclose(f_id);
+endfunction: final_phase
+
 
 
